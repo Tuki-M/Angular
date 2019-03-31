3793cd4db51b7e46ff07c562bfbc8ef91c6aa688
Tuki-M <men-inttic@hotmail.fr>
2019-04-01 00:25:35 +0200

    [APM tutorial] Source code of the course - modules [Components - Templates, Interpolation, and Directives - Data Binding & Pipes - Nested Components - Services and Dependency Injection - Retrieving Data Using HTTP]

---
+++ APM/README.md
@@ -0,0 +1,7 @@
+# APM tutorial
+
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 7.0.3.
+
+thanks to the tutorial of @Deborah Kurata(https://app.pluralsight.com/library/courses/angular-2-getting-started-update/table-of-contents)
+
+
---
+++ APM/src/app/products/product-list.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
+import { ProductService } from "./product.service";
+
+@Component({
+    selector:'pm-products',
+    templateUrl:'./product-list.component.html',
+    styleUrls:['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    errorMessage:string;
+    _listFilter: string;
+
+    public get listFilter(): string {
+        return this._listFilter;
+    }
+    public set listFilter(value: string) {
+        this._listFilter = value;
+        this.filtredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+    
+    filtredProducts:IProduct[];
+    products: IProduct[] ;
+
+
+    constructor(private productService: ProductService){
+
+    }
+
+    toggleImage(): void{
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log('In OnInit');
+        this.productService.getProducts().subscribe(
+            products => {
+                this.products = products;
+                this.filtredProducts = this.products;
+            },
+            error => this.errorMessage = <any>error
+        );       
+    }
+
+    performFilter(filtrerBy: string): IProduct[] {
+        filtrerBy = filtrerBy.toLocaleLowerCase();
+        return this.products.filter((product: IProduct) => product.productName.toLocaleLowerCase().indexOf(filtrerBy) !== -1);
+    }
+      
+    onRatingClicked(message: string):void{
+        this.pageTitle = 'Product List : ' + message;
+    }
+}
